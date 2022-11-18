0831a84741f08a1798c8ab3257f43e05e9a69c8a
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-18 17:06:04 +0900

    cssファイルの作成

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
-<?php
-echo "hello";
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>PHP掲示板</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
