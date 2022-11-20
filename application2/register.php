f61ebcb385093d60b126fab468b608a15ba2e396
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-20 15:30:23 +0900

    正規表現→/\A[a-z\d]{8,100}+\z/i

--- application2/register.php
+++ application2/register.php
@@ -1,4 +1,22 @@
 <?php
+
+//エラーメッセージ
+$err = [];
+
+//バリデーション機能
+if (!$username = filter_input(INPUT_POST, 'username')) {
+  $err[] = 'ユーザ名を記入してください';
+}
+if (!$username = filter_input(INPUT_POST, 'email')) {
+  $err[] = 'メールアドレスを記入してください';
+}
+
+$password = filter_input(INPUT_POST, 'password');
+//正規表現→"/\A[a-z\d]{8,100}+\z/i"
+if(preg_match("/\A[a-z\d]{8,100}+\z/i",$password))
+$password_conf = filter_input(INPUT_POST, 'password_conf');
+
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
