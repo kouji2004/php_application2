63e9f5dad7055ad7e8852254f8a61e77621c5396
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-20 15:24:05 +0900

    html ユーザ登録画面

---
+++ application2/register.php
@@ -0,0 +1,18 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ユーザ登録完了画面</title>
+</head>
+
+<body>
+  <p>ユーザ登録が完了しました</p>
+  <a href="./signup_form.php">戻る</a>
+</body>
+
+</html>
